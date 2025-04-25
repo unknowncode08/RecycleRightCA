cf3e57c8562112917c12aec7c57c3a3e4e55bc2f
unknowncode08 <91484006+unknowncode08@users.noreply.github.com>
2025-04-24 18:45:42 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+// Simple offline cache (index + mobile)
+const CACHE_NAME = 'recyclerightca-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/mobile',
+  '/mobile.html',
+  '/manifest.webmanifest',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  if (evt.request.method !== 'GET') return;
+  evt.respondWith(
+    caches.match(evt.request).then((cached) => cached || fetch(evt.request))
+  );
+});
