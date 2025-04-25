e9465ac7ac14575edcbcc3bbb0e803482040cb05
unknowncode08 <91484006+unknowncode08@users.noreply.github.com>
2025-04-24 18:50:51 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,13 @@
-// Simple offline cache (index + mobile)
-const CACHE_NAME = 'recyclerightca-v1';
-const ASSETS = [
+const CACHE_NAME='recyclerightca-v1';
+const ASSETS=[
   '/',
   '/index.html',
-  '/mobile',
-  '/mobile.html',
+  '/mobile/',
+  '/mobile/index.html',
   '/manifest.webmanifest',
   '/icons/icon-192.png',
   '/icons/icon-512.png'
 ];
-
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (evt) => {
-  if (evt.request.method !== 'GET') return;
-  evt.respondWith(
-    caches.match(evt.request).then((cached) => cached || fetch(evt.request))
-  );
-});
+self.addEventListener('install',evt=>evt.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))).then(()=>self.skipWaiting()));
+self.addEventListener('activate',evt=>evt.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))).then(()=>self.clients.claim()));
+self.addEventListener('fetch',evt=>{if(evt.request.method!=='GET')return;evt.respondWith(caches.match(evt.request).then(cached=>cached||fetch(evt.request)));});
