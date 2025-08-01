8252cd67a5f8516eb168ae787f2e67a9a63d0da6
Omar Ahmad <oesoliman@gmail.com>
2025-08-01 12:07:34 -0700

    updated main website look

--- sw.js
+++ sw.js
@@ -1,11 +1,4 @@
-const CACHE_NAME = 'recyclerightca-v2';
-const ASSETS = [
-    '/mobile/',
-    '/mobile/index.html',
-    'manifest.webmanifest',
-    'icons/icon-192.png',
-    'icons/icon-512.png'
-];
-self.addEventListener('install', evt => evt.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))).then(() => self.skipWaiting()));
-self.addEventListener('activate', evt => evt.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))).then(() => self.clients.claim()));
-self.addEventListener('fetch', evt => { if (evt.request.method !== 'GET') return; evt.respondWith(caches.match(evt.request).then(cached => cached || fetch(evt.request))); });
+// This empty event listener is all that's needed to meet the PWA install criteria.
+self.addEventListener('fetch', (event) => {
+  // This service worker is for PWA installability only.
+});
