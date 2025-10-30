4f76cc5672ca80decc86915ec1e98b72d914495a
oesoliman <oesoliman@gmail.com>
2025-10-30 17:47:17 +0000

    debug

--- mobile/script.js
+++ mobile/script.js
@@ -1157,6 +1157,7 @@ Examples:
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(body)
         });
+        alert(res.json());
         const data = await res.json();
         const text = data.candidates[0].content.parts[0].text.trim();
         alert('it works?');
